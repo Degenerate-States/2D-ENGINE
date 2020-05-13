356d2689f79038466187bbe43a4249fcb484e130
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2020-05-13 13:00:57 -0400

    removed engine from header file to prevent dupe instances

--- include/engine.h
+++ include/engine.h
@@ -27,39 +27,3 @@ extern void Render(double dt);
 
 extern void Events(SDL_Event* event, Screen* screen,double dt);
 extern void Keys(const Uint8* keys,Screen* screen,double dt);
-
-
-class Engine{
-    public:
-        int fps;
-        //seconds per frame, used as internal time step dt
-        double spf;
-        int tickNumber;
-        bool running;
-        
-        SDL_Event event;
-        SDL_GLContext gl_context;
-        Screen screen;
-        // holds state of current keys
-        const Uint8* keys;
-
-        void init(Config* cfg, Assets* assets,Stats* stats);
-        void events(double dt);
-        void render();
-        //also increases ticknumber
-        void fixFramerate();
-        void gameLoop();
-        void clean();
-    
-    private:
-        //both used by fixFramerate
-        Uint32 frameTime;
-        //miliseconds per frame
-        int mspf;
-
-        //used in init,returns window and renderer also assigns gl_context
-        tuple<SDL_Window*,SDL_Renderer*> SDL_Visuals_Boilerplate(Config* cfg);
-
-        void Check_Quit();
-
-};
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,5 +1,40 @@
 #include "engine.h"
 
+class Engine{
+    public:
+        int fps;
+        //seconds per frame, used as internal time step dt
+        double spf;
+        int tickNumber;
+        bool running;
+        
+        SDL_Event event;
+        SDL_GLContext gl_context;
+        Screen screen;
+        // holds state of current keys
+        const Uint8* keys;
+
+        void init(Config* cfg, Assets* assets,Stats* stats);
+        void events(double dt);
+        void render();
+        //also increases ticknumber
+        void fixFramerate();
+        void gameLoop();
+        void clean();
+    
+    private:
+        //both used by fixFramerate
+        Uint32 frameTime;
+        //miliseconds per frame
+        int mspf;
+
+        //used in init,returns window and renderer also assigns gl_context
+        tuple<SDL_Window*,SDL_Renderer*> SDL_Visuals_Boilerplate(Config* cfg);
+
+        void Check_Quit();
+
+};
+
 
 int main(int argc, char **argv) {
     Assets* assets = new Assets();
@@ -25,6 +60,9 @@ int main(int argc, char **argv) {
     engine.clean();
     return 0;
 }
+
+
+
 
 //************************//
 //**  Engine Methods    **//
